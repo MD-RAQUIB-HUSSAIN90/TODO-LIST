2f9aed8be53bd99bee5fa31cc5186e12b9fdb8c7
MD RAQUIB HUSSAIN <mdraquibhussain71@gmail.com>
2026-06-04 17:59:00 +0530

    last commit

--- script.js
+++ script.js
@@ -1,34 +1,47 @@
 let input = document.querySelector("input");
 let btn = document.querySelector("button");
 let ul = document.querySelector("ul");
+let child = document.querySelector(".child");
 
 // ADD FUNCTIONALITY
 btn.addEventListener("click", function () {
-  let li = document.createElement("li");
-  if (input.value === "") {
+  if (input.value.trim() === "") {
     alert("Please enter a task");
     return;
-    
   }
+  let li = document.createElement("li");
+  let span = document.createElement("span");
+  span.textContent = input.value;
+  li.classList.add("list-item");
+  li.prepend(span);
+  // CREATE DELETE BUTTON
 
+  let deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "Delete";
+  deleteBtn.classList.add("delete");
+  let got = document.createElement("button");
+  got.textContent = "Got";
+  got.classList.add("got");
+  let div = document.createElement("div");
+  div.classList.add("btn-box");
+  li.append(div);
 
+  // APPEND DELETE BUTTON TO LI
+  div.appendChild(got);
+  div.appendChild(deleteBtn);
 
-  else {
-    li.textContent = input.value;
-    li.classList.add("list-item");
-    // CREATE DELETE BUTTON
-    let deleteBtn = document.createElement("button");
-    deleteBtn.textContent = "Delete";
-    deleteBtn.classList.add("delete");
-    // APPEND DELETE BUTTON TO LI
-   
-    li.appendChild(deleteBtn);
-    ul.appendChild(li);
+  ul.appendChild(li);
 
-    // DELETE FUNCTIONALITY
-    deleteBtn.addEventListener("click", function () {
-      li.remove();
-    });
-    input.value = "";
-  }
+  // DELETE FUNCTIONALITY
+
+  deleteBtn.addEventListener("click", function () {
+    li.remove();
+  });
+
+  got.addEventListener("click", function () {
+    span.style.textDecoration = "line-through";
+    span.style.color = "grey";
+  });
+
+  input.value = "";
 });
